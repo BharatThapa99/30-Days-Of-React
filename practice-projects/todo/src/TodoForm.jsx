3aae21a0c0b320fe42ebb88c0bfac66c99276529
bharat <thapabharat123456@gmail.com>
2024-01-27 14:09:34 +0545

    little bit of tailwid css

--- practice-projects/todo/src/TodoForm.jsx
+++ practice-projects/todo/src/TodoForm.jsx
@@ -20,7 +20,7 @@ export function TodoForm({ onSubmit}) {
           <label htmlFor="item" className="block text-lg font-medium leading-6 text-gray-900 mt-5">Add New Item</label><br/>
            <input  value={newItem} onChange={e => setNewItem(e.target.value)} type="text" name="" id="item" className="w-2/4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         
-          <button type="submit" class="rounded-md ml-5 p-4 bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add</button>
+          <button type="submit" className="rounded-md ml-5 p-4 bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add</button>
         
         </div>
       </form>
--- practice-projects/todo/src/TodoItem.jsx
+++ practice-projects/todo/src/TodoItem.jsx
@@ -1,17 +1,17 @@
 export default function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
     return (
-      <li>
-        <label className="text-lg hover:text-xl font-medium">
-          <input
-            type="checkbox"
-            checked={completed}
-            onChange={e => toggleTodo(id, e.target.checked)}
-          />
-          {title}
-        </label>
-        {/* <button onClick={() => deleteTodo(id)} className="btn btn-danger">
-          Delete
-        </button> */} 
-      </li>
+      <div className="flex items-center mb-4">
+    <input id={id} type="checkbox" value="" 
+    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+    checked={completed}
+    onChange={e => toggleTodo(id, e.target.checked)}
+      />
+    <label htmlFor={id}
+    className="ms-2 text-sm font-medium text-gray-900">
+      {title}</label>
+</div>
+      
+          
+     
     )
   }
